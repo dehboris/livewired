085c453962a92cc09988235f0a7cc0c722596c55
Brian Faust <hello@basecode.sh>
2020-02-29 17:17:26 +0200

    add team and user photo components

---
+++ src/Components/Teams/UpdateTeamPhoto.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Livewired.
+ *
+ * (c) KodeKeep <hello@kodekeep.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace KodeKeep\Livewired\Components\Teams;
+
+use KodeKeep\Livewired\Components\Component;
+use KodeKeep\Livewired\Components\Concerns\InteractsWithTeam;
+
+class UpdateTeamPhoto extends Component
+{
+    use InteractsWithTeam;
+}
